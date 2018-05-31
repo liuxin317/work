2106a27295ce96a8ebee07876fa16e225755338b
keyus <6325610@qq.com>
2018-05-31 19:03:03 +0800

    更新

---
+++ node/openFile.js
@@ -0,0 +1,11 @@
+// 打开文件
+var fs = require('fs');
+
+// 异步打开
+console.log('准备打开文件！');
+fs.open('input.txt', 'r+', function(err, fd) {
+  if (err) {
+    return console.error(err);
+  }
+  console.log('文件打开成功！')
+});
